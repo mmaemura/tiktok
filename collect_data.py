eb726229a43d10c81a746203fcbbf5855820a80c
Matthew Maemura <matthew.maemura@gmail.com>
2022-02-05 21:59:21 -0800

    audio transcription, to db file working

--- collect_data.py
+++ collect_data.py
@@ -5,7 +5,9 @@ import random
 import datetime
 import requests
 from pydub import AudioSegment
+import speech_recognition as sr
 import os
+import sqlite3
 
 
 ##to run
@@ -17,7 +19,7 @@ import os
 ##cd ..
 ##python collect_data.py
 
-def get_tiktok_data(k):
+def get_tiktok_data(k = 1, get_transcription = False, to_db = False, to_csv = False):
     verifyFp = "verify_5ac3d7e61b6197794299a7ffda73bc33"
 
     api = TikTokApi.get_instance(custom_verifyFp=verifyFp, use_test_endpoints=True)
@@ -26,6 +28,8 @@ def get_tiktok_data(k):
 
 
     #time.sleep(1) to wait a sec
+
+    conn = sqlite3.connect("tiktok.db")
 
     columns = ["date_pulled","id", "video_title", "video_url", 'upload_time',"creator", "creator_nickname", "creator_id", "creator_verified", "like", "share", "comment", "view", "original_item", "sound_id", "sound_title", "sound_author", "sound_orignal", "sound_url", "sound_transcribed"]
     df = pd.DataFrame(columns = columns)
@@ -39,43 +43,62 @@ def get_tiktok_data(k):
         misc = [tiktok['originalItem']]
         sound = [tiktok['music']['id'], tiktok['music']['title'], tiktok['music']['authorName'], tiktok['music']['original'], tiktok['music']['playUrl'], ""]
 
-        row = date + video + creator + stats + misc + sound
+        if get_transcription:
+            id = video[0]
+            py_file_path = os.path.dirname(os.path.realpath(__file__)) #full path to directory of this python file
+            mp3_folder = r'/mp3_files/'
+            mp3_file = f'{id}.mp3'
+            mp3_path = py_file_path + mp3_folder + mp3_file
 
-        download_mp3(id = video[0], url = sound[4])
-        get_audio_transcription(id = video[0])
+            wav_folder = '\wav_files\\'
+            wav_file = f'{id}.wav'
+            wav_path = py_file_path + wav_folder + wav_file
+
+            download_mp3(mp3_path, url = sound[4])
+            mp3_to_wav(mp3_path, wav_path)
+            sound[-1] = get_audio_transcription(wav_path)
+
+        row = date + video + creator + stats + misc + sound
 
         df.loc[i] = row
         i += 1
+        if i % 10 == 0:
+            print(i)
 
-    #df.to_csv("first_csv.csv")
-    return df.to_csv("third_csv.csv")
+    if to_db:
+        df.to_sql("tiktok", conn, if_exists = "append", index = False)
+
+    if to_csv:
+        return df.to_csv("{date[0]}.csv")
 
 
-def download_mp3(id, url):
-    py_file_path = os.path.dirname(os.path.realpath(__file__)) #full path to directory of this python file
-    mp3_folder = r'/mp3_files/'
-    mp3_file = f'{id}.mp3'
-    full_path = py_file_path + mp3_folder + mp3_file
-    
+def download_mp3(mp3_path, url):
     r = requests.get(url)
-    with open(full_path , 'wb') as f:
+    with open(mp3_path, 'wb') as f:
         f.write(r.content)
 
-def get_audio_transcription(id):
-    py_file_path = os.path.dirname(os.path.realpath(__file__))
-    wav_folder = '\wav_files\\'
-    mp3_folder = '\mp3_files\\'
-    mp3_file = f'{id}.mp3'
-    wav_file = f'{id}.wav'
-
-    mp3_file_path = py_file_path + mp3_folder + mp3_file
-    if not os.path.exists(mp3_file_path):
-        print('asdlkfjasldkfjaslkdfjlasdjkfflkasjdflkajsdflkjasdflkjasdlkfjasldkfjlasdkjflksda')
-    wav_file_path = py_file_path + wav_folder + wav_file
-
-    print("file path is: ", mp3_file_path)
+def mp3_to_wav(mp3_path, wav_path):
+    try:    
+        audio = AudioSegment.from_mp3(mp3_path)
+        audio.export(wav_path, format = "wav")
+    except Exception as e:
+        print(e)
     
-    audio = AudioSegment.from_mp3(mp3_file_path)
-    audio.export(wav_file_path, format = "wav")
 
-get_tiktok_data(5)
+def get_audio_transcription(wav_path):
+    exists = os.path.isfile(wav_path)
+    if exists:
+        r = sr.Recognizer()
+        with sr.AudioFile(wav_path) as source:
+            audio = r.record(source)
+        
+        try:
+            return r.recognize_google(audio)
+        except sr.UnknownValueError:
+            return "NA"
+        except sr.RequestError as e:
+            print("Google error; {0}".format(e))
+    else:
+        return "NA"
+
+get_tiktok_data(k = 100, get_transcription = True, to_db = True, to_csv = False)
