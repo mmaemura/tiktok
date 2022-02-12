2cdf0d3ec02d39933964572ecc25afc65043ead5
Matthew Maemura <matthew.maemura@gmail.com>
2022-02-11 19:40:13 -0800

    day 6 of query

--- run_query.py
+++ run_query.py
@@ -8,9 +8,9 @@ from query_functions import *
 ##cd ..
 ##cd ..
 ##python run_query.py
-#get_tiktok_data(k = 100, get_transcription = True, to_db = True, db_file_name = "tiktok.db", to_csv = False)
+get_tiktok_data(k = 100, get_transcription = True, to_db = True, db_file_name = "tiktok.db", to_csv = False)
 
 
 #Getting Our Data
 
-get_tiktok_data(k = 10, get_transcription=True, to_db=True, db_file_name="test.db", to_csv=False)
+#get_tiktok_data(k = 10, get_transcription=True, to_db=True, db_file_name="test.db", to_csv=False)
