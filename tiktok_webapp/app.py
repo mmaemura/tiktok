2632bf755ad95c1a077414d3d8883bfc37790949
emmakbradley <emmabradley@g.ucla.edu>
2022-02-28 13:06:12 -0800

    users can now enter how many days they would like to query

--- tiktok_webapp/app.py
+++ tiktok_webapp/app.py
@@ -13,6 +13,32 @@ import sqlite3
 from flask import Flask, render_template, request, g
 app = Flask(__name__)
 
+def get_tiktok_db():
+    g.tiktok_db = sqlite3.connect("tiktok.db")
+    g.tiktok_db.execute('''CREATE TABLE IF NOT EXISTS tiktoks(id INTEGER, video_title TEXT, sound_transcribed TEXT, upload_time TEXT, view INTEGER, date_pulled TEXT)''')
+    return g.tiktok_db
+
+def plot_sentiments(days):
+    # connetct to db
+    g.tiktok_db = get_tiktok_db()
+
+     # initialize cursor
+    cursor = g.tiktok_db.cursor()
+
+    # get tik toks from past # of days
+    cmd = \
+    f"""
+    SELECT id, video_title, sound_transcribed, upload_time, view, date_pulled
+    FROM tiktok
+    """ 
+
+    df = pd.read_sql_query(cmd, cursor)
+
+    # close database
+    g.message_db.close()
+
+    return none
+
 @app.route("/")
 def main():
     return render_template('base.html')
